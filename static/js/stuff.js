06d6b493fbd24db25647cb0085dd5c8c901c9f52
Nat Welch <nat@natwelch.com>
2010-09-23 19:28:22 -0700

    whoops.

---
+++ static/js/stuff.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   var values = {};
+   $('#theForm').submit(function() {
+      $.each($('#theForm').serializeArray(), function(i, field) {
+         values[field.name] = field.value;
+      });
+
+      $.post("/", values, function(data) {
+         console.log(data);
+      });
+
+      // to prevent an actual reload.
+      return false;
+   });
+});
